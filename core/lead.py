13c7dacbd87898d8912024c94e041010f368a502
Supun Buddhima Dharmaratne <It24101504@my.sliit.lk>
2026-06-28 18:41:30 +0530

    feat: create Lead model

--- core/lead.py
+++ core/lead.py
@@ -1,40 +1,20 @@
-from datetime import datetime
-
-
 class Lead:
 
-    def __init__(self, name, company, industry, role="", website="", location="", score=0):
+    def __init__(self, name, website, description, location):
         self.name = name
-        self.company = company
-        self.industry = industry
-        self.role = role
         self.website = website
+        self.description = description
         self.location = location
-        self.score = score
-        self.outreach_email = ""
+        self.score = 0
         self.notes = []
-        self.found_at = datetime.now().isoformat()
-
-    def add_note(self, note):
-        self.notes.append(note)
-
-    def is_qualified(self):
-        return self.score >= 60
 
     def to_dict(self):
-        return {
+        return{
             "name": self.name,
-            "company": self.company,
-            "industry": self.industry,
-            "role": self.role,
-            "email": "",
             "website": self.website,
+            "description": self.description,
+            "location":self.location,
             "score": self.score,
-            "notes": self.notes,
-            "outreach_email": self.outreach_email,
-            "found_at": self.found_at
+            "notes": self.notes
         }
 
-    def __str__(self):
-        status = "QUALIFIED" if self.is_qualified() else "not qualified"
-        return f"{self.name} @ {self.company} | Score: {self.score} | {status}"
